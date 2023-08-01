3f30a5800254dfea82f315bb4456255abc8ddf78
Timur <64714613+Akhmadulin@users.noreply.github.com>
2023-08-01 20:16:38 +0500

    Update resource_notification.py

--- resource_notification.py
+++ resource_notification.py
@@ -3,8 +3,14 @@ import requests
 import time
 
 # Замените YOUR_BOT_TOKEN на токен вашего телеграм-бота
-BOT_TOKEN = "6552760294:AAHHB5qJuSjGFdFZnt1F65ivG4aEB1o8U1A"
-CHAT_ID = "1053452769"  # Замените на ID вашего телеграм-чата или пользователя
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+CHAT_ID = "YOUR_CHAT_ID"  # Замените на ID вашего телеграм-чата или пользователя
+
+# Переменные для хранения предыдущих значений нагрузки
+previous_cpu_usage = None
+previous_memory_usage = None
+previous_swap_usage = None
+previous_disk_usage = None
 
 def send_message(message):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
@@ -21,54 +27,36 @@ def get_uptime():
         return uptime_minutes
 
 def check_resource_usage():
+    global previous_cpu_usage, previous_memory_usage, previous_swap_usage, previous_disk_usage
+
     while True:
         # Мониторинг загрузки CPU
         cpu_usage = psutil.cpu_percent(interval=1)
-        if cpu_usage > 70:
-            send_message(f"Внимание! Загрузка CPU превышает 70%: {cpu_usage}%")
-        if cpu_usage > 80:
-            send_message(f"Внимание! Загрузка CPU превышает 80%: {cpu_usage}%")
-        if cpu_usage > 90:
-            send_message(f"Внимание! Загрузка CPU превышает 90%: {cpu_usage}%")
-        if cpu_usage == 100:
-            send_message(f"Критическое состояние! Загрузка CPU достигла 100%: {cpu_usage}%")
+        if previous_cpu_usage is not None and cpu_usage < previous_cpu_usage:
+            send_message(f"Внимание! Загрузка CPU снизилась до {cpu_usage}%")
+        previous_cpu_usage = cpu_usage
 
         # Мониторинг использования памяти
         memory_usage = psutil.virtual_memory().percent
-        if memory_usage > 70:
-            send_message(f"Внимание! Использование памяти превышает 70%: {memory_usage}%")
-        if memory_usage > 80:
-            send_message(f"Внимание! Использование памяти превышает 80%: {memory_usage}%")
-        if memory_usage > 90:
-            send_message(f"Внимание! Использование памяти превышает 90%: {memory_usage}%")
-        if memory_usage == 100:
-            send_message(f"Критическое состояние! Использование памяти достигло 100%: {memory_usage}%")
+        if previous_memory_usage is not None and memory_usage < previous_memory_usage:
+            send_message(f"Внимание! Использование памяти снизилось до {memory_usage}%")
+        previous_memory_usage = memory_usage
 
         # Мониторинг использования swap
         swap_usage = psutil.swap_memory().percent
-        if swap_usage > 70:
-            send_message(f"Внимание! Использование swap превышает 70%: {swap_usage}%")
-        if swap_usage > 80:
-            send_message(f"Внимание! Использование swap превышает 80%: {swap_usage}%")
-        if swap_usage > 90:
-            send_message(f"Внимание! Использование swap превышает 90%: {swap_usage}%")
-        if swap_usage == 100:
-            send_message(f"Критическое состояние! Использование swap достигло 100%: {swap_usage}%")
+        if previous_swap_usage is not None and swap_usage < previous_swap_usage:
+            send_message(f"Внимание! Использование swap снизилось до {swap_usage}%")
+        previous_swap_usage = swap_usage
 
         # Мониторинг использования жесткого диска
         disk_usage = psutil.disk_usage('/').percent
-        if disk_usage > 70:
-            send_message(f"Внимание! Использование места на жестком диске превышает 70%: {disk_usage}%")
-        if disk_usage > 80:
-            send_message(f"Внимание! Использование места на жестком диске превышает 80%: {disk_usage}%")
-        if disk_usage > 90:
-            send_message(f"Внимание! Использование места на жестком диске превышает 90%: {disk_usage}%")
-        if disk_usage == 100:
-            send_message(f"Критическое состояние! Использование места на жестком диске достигло 100%: {disk_usage}%")
+        if previous_disk_usage is not None and disk_usage < previous_disk_usage:
+            send_message(f"Внимание! Использование места на жестком диске снизилось до {disk_usage}%")
+        previous_disk_usage = disk_usage
 
         # Отправка уведомления при загрузке сервера
         uptime_minutes = get_uptime()
-        if uptime_minutes < 1:
+        if uptime_minutes < 2:
             send_message("Сервер был перезагружен.")
 
         time.sleep(60)  # Проверка каждые 60 секунд
