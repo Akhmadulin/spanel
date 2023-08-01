2da89e165ff4a34b2b06be1ffbfa169d7b94db65
Timur <64714613+Akhmadulin@users.noreply.github.com>
2023-08-01 19:01:22 +0500

    Update lemp_installer.py
    moved mysql installer to different file

--- lemp_installer.py
+++ lemp_installer.py
@@ -9,29 +9,6 @@ def install_lemp():
     # Установка Nginx
     run_command("sudo apt update")
     run_command("sudo apt install nginx -y")
-    
-    # Установка MySQL
-
-def install_mysql():
-    try:
-        # Обновление списка пакетов и установка MySQL
-        subprocess.run(["sudo", "apt", "install", "mysql-server", "-y"])
-
-        # Сохранение пароля MySQL в файл
-        password = "A5392420t"  # Замените на свой пароль
-        with open("mysql_password.txt", "w") as f:
-            f.write(password)
-
-        # Запуск MySQL и добавление автозапуска
-        subprocess.run(["sudo", "systemctl", "start", "mysql"])
-        subprocess.run(["sudo", "systemctl", "enable", "mysql"])
-
-        # Настройка безопасности MySQL (простой вариант без команды mysql_secure_installation)
-        subprocess.run(f"sudo mysql -e \"ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '{password}';\"")
-
-        print("MySQL успешно установлен и настроен. Пароль сохранен в файл 'mysql_password.txt'")
-    except Exception as e:
-        print("Ошибка при установке MySQL:", e)
 
     # Установка PHP
     run_command("sudo apt install php7.4-fpm php7.4-mysql php7.4-mbstring php7.4-xml php7.4-bcmath php7.4-gd php7.4-zip -y")
@@ -51,6 +28,7 @@ def install_mysql():
 
     #Установка дополнительных утилит
     run_command("sudo apt install net-tools mc htop -y")
+    run command("python3 mysql.py")
     
     print("LEMP установлен и настроен успешно!")
 
