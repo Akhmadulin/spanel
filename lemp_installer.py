1d640846274156a347055034da5f67980675f96b
Timur <64714613+Akhmadulin@users.noreply.github.com>
2023-08-16 18:13:26 +0500

    Update lemp_installer.py
    addet certbot and new php extensions

--- lemp_installer.py
+++ lemp_installer.py
@@ -7,18 +7,21 @@ def run_command(command):
     return os.system(command)
 
 def install_lemp():
+    #Установка дополнительных утилит
+    run_command("sudo apt install net-tools mc htop zip unzip p7zip-full dialog certbot python3-certbot-nginx -y")
+    
     # Установка Nginx
     run_command("sudo apt update")
     run_command("sudo apt install nginx -y")
 
     # Установка PHP
-    run_command("sudo apt install php8.1 php8.1-{fpm,cli,bcmath,bz2,curl,intl,gd,mbstring,mysql,zip,xml} -y")
+    run_command("sudo apt install php8.1 php8.1-{fpm,curl,pdo,cli,bcmath,bz2,curl,intl,gd,mbstring,mysql,zip,xml} -y")
   
     # Настройка PHP-FPM
     php_fpm_conf_file = '/etc/php/8.1/fpm/pool.d/www.conf'  # Путь к файлу конфигурации PHP-FPM (может отличаться в разных версиях Ubuntu)
     with open(php_fpm_conf_file, 'r') as f:
         php_conf = f.read()
-    php_conf = php_conf.replace('www-data', 'nginx')
+    php_conf = php_conf.replace('www-data', 'www-data')
     with open(php_fpm_conf_file, 'w') as f:
         f.write(php_conf)
     # Вызов функции установки mysql
@@ -29,8 +32,7 @@ def install_lemp():
     run_command("sudo systemctl restart php8.1-fpm")
     run_command("sudo systemctl restart mysql")
 
-    #Установка дополнительных утилит
-    run_command("sudo apt install net-tools mc htop zip unzip p7zip-full -y")
+
     print("LEMP установлен и настроен успешно!")
 
 if __name__ == "__main__":
